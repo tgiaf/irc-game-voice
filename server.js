63e6b8d9e7d6f047ceae66225754453c96046169
tgiaf <43760759+tgiaf@users.noreply.github.com>
2025-12-23 13:04:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,15 +8,13 @@ const io = new Server(server, {
     cors: { origin: "*" }
 });
 
-// OdaAdı -> [SocketID, SocketID...]
-const rooms = {};
-
 io.on('connection', (socket) => {
-    console.log('Oyuncu bağlandı:', socket.id);
+    console.log('Bağlantı:', socket.id);
 
-    socket.on('join_room', (roomId) => {
-        // Odaya katıl
+    // GÜNCELLENDİ: Artık nick bilgisini de alıyoruz
+    socket.on('join_room', ({ roomId, nick }) => {
         socket.join(roomId);
+        socket.nickname = nick; // Nicki sokete kaydet
         
         // Odadaki diğer kişileri bul
         const clients = io.sockets.adapter.rooms.get(roomId);
@@ -25,40 +23,41 @@ io.on('connection', (socket) => {
         if (clients) {
             clients.forEach(clientId => {
                 if (clientId !== socket.id) {
-                    otherUsers.push(clientId);
+                    const clientSocket = io.sockets.sockets.get(clientId);
+                    otherUsers.push({ 
+                        id: clientId, 
+                        nick: clientSocket ? clientSocket.nickname : "Bilinmiyor" 
+                    });
                 }
             });
         }
 
-        // Katılan kişiye "Odada bunlar var, onlara bağlan" de
+        // 1. Katılan kişiye odadakilerin listesini gönder
         socket.emit('all_users', otherUsers);
+
+        // 2. Odadakilere "Yeni biri geldi" de (Nick ile)
+        socket.to(roomId).emit('user_joined', { id: socket.id, nick: nick });
     });
 
-    // WebRTC Sinyal İletişimi (Kişiden Kişiye)
+    // WebRTC Sinyalleri (Değişmedi)
     socket.on('offer', (data) => {
-        io.to(data.target).emit('offer', {
-            sdp: data.sdp,
-            caller: socket.id
-        });
+        io.to(data.target).emit('offer', { sdp: data.sdp, caller: socket.id });
     });
 
     socket.on('answer', (data) => {
-        io.to(data.target).emit('answer', {
-            sdp: data.sdp,
-            responder: socket.id
-        });
+        io.to(data.target).emit('answer', { sdp: data.sdp, responder: socket.id });
     });
 
     socket.on('ice_candidate', (data) => {
-        io.to(data.target).emit('ice_candidate', {
-            candidate: data.candidate,
-            sender: socket.id
-        });
+        io.to(data.target).emit('ice_candidate', { candidate: data.candidate, sender: socket.id });
     });
 
-    socket.on('disconnect', () => {
-        console.log('Oyuncu ayrıldı:', socket.id);
-        // Odadakilere haber ver (Gerekirse UI güncellemesi için)
+    // Ayrılma Durumu
+    socket.on('disconnecting', () => {
+        // Hangi odalardaysa oradakilere haber ver
+        for (const room of socket.rooms) {
+            socket.to(room).emit('user_left', { id: socket.id });
+        }
     });
 });
 
